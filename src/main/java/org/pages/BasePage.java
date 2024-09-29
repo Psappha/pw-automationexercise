8c5bf3ad8aab9b3459947aa5023ad1da43f1f23a
Pablo Soler <pablosoler1983@gmail.com>
2024-09-29 12:35:05 -0300

    Add Cart page and methods Add Modal and methods Add methods for interacting with product card Fix warnings
    Signed-off-by: Pablo Soler <pablosoler1983@gmail.com>

--- src/main/java/org/pages/BasePage.java
+++ src/main/java/org/pages/BasePage.java
@@ -1,6 +1,7 @@
 package org.pages;
 
 import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.AriaRole;
 
 public class BasePage<T extends BasePage<T>> {
 
@@ -9,7 +10,7 @@ public class BasePage<T extends BasePage<T>> {
     static Browser browser = playwright.webkit().launch(options);
     public static Page page = browser.newPage();
 
-    private final Locator productsLink = page.locator("[href='/products']");
+    private final Locator productsLink = page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName(" Products"));
 
     public ProductsPage goToProducts() {
         productsLink.click();
